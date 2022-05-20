4abf3a0fcfc46a4b3f6a52d1a900e8adcaa2e2dc
Uoo0 <wjsrkf13@khu.ac.kr>
2022-05-20 11:19:09 +0900

    2022한솔 인턴십

---
+++ scheduler-api-project/scheduler-api-web/src/assets/Cookie.js
@@ -0,0 +1,29 @@
+import Cookies from "universal-cookie";
+//import Math
+
+const cookies = new Cookies();
+
+
+class Cookie{
+
+    setCookie(name, value) {
+        return cookies.set(name, value, {
+            path : '/',
+            maxAge: 6 * 60* 60,
+            secure: true,
+            // httpOnly: true
+        });
+    }
+      
+    getCookie(name) {
+        return cookies.get(name);
+    }
+    
+    removeCookie(name) {
+        return cookies.remove(name, {path : '/'});
+    }
+      
+}
+
+
+export default new Cookie();
